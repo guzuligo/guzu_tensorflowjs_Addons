c95e9df1182acde58ce85c43b6ba414072973ef3
guzuWin <guzuligo@gmail.com>
2021-01-04 10:33:24 +0300

    drawPixelsToCanvas

--- GuzuDataTools.js
+++ GuzuDataTools.js
@@ -402,30 +402,30 @@ class GuzuFileTools {
         
     }
     
-    //TODO:WIP
+    //TODO:further testing
     drawPixelsToCanvas(pixels,canvas,options_){
         if(typeof(canvas)==='string')
             canvas=document.getElementById(canvas);
         var c=canvas.getContext('2d');
         //default
         options_=options_||{};
-        var defOptions={x:0,y:0,w:c.width,h:c.height,divide:1,add:0,func:null};//func need to be inverted
+        var defOptions={x:0,y:0,w:canvas.width,h:canvas.height,divide:1,add:0,func:null};//func need to be inverted
         for (var i in defOptions) if(!options_[i])options_[i]=defOptions[i];
         var o=options_;
         var c;
         var c2=(c).getImageData(o.x,o.y,o.w,o.h);
-        
+        if(!o.c)o.c=2**pixels[0][0].length-1;
+        pixels=(pixels).flat().flat();
         for(var i in pixels){
-            for(var ii=0;ii<pixels[i].length;ii++){
-                if(options_.func)pixels[i]=options_.func(pixels[i]);
-                pixels[i]=(pixels[i]-options_.add)*options_.divide;
-            }
-            while(pixels[i].length<3)pixels[i].push(pixels[i][0]);
-            if(pixels[i].length<4)pixels[i].push(255);
+            if(options_.func)
+                pixels[i]=options_.func(pixels[i]);
+            pixels[i]=(pixels[i]-options_.add)*options_.divide;
+            
         }
-
-        this.NNSetImage(pixels,c2,{w:o.w,h:o.h,c:o.c,c:4});
+        var flat=pixels;//.flat().flat();
+        this.NNSetImage(flat,c2,{w:o.w,h:o.h,c:o.c,});
         c.putImageData(c2,0,0);
+        return flat;
     }
 
     //returns a part of the array src_ looped
